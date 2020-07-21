3fece710646b685e324f875a1fc65271d007c96e
Rebecca <gitlab@indoctrine.com>
2020-07-21 21:19:13 +1000

    Refactor. Added connection pooling, pb, and hugs

--- bot.py
+++ bot.py
@@ -5,29 +5,35 @@ import logging
 import sys
 import mysql.connector
 from mysql.connector import Error
+from mysql.connector import pooling
 import atexit
-import streak
+from streak import Streaks
+
+CMD_COOLDOWN = 86400 # Default cooldown is 24 hours
+DB_NAME = 'streakbot'
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', stream=sys.stderr,level=logging.INFO)
 
 @atexit.register
 def cleanup():
-    db_conn.close()
+    logging.info('Shutting down...')
 
-def create_db_connection(host_name, user_name, user_password, db):
-    connection = None
+def create_db_connpool(host_name, user_name, user_password, db):
+    connection_pool = None
     try:
-        connection = mysql.connector.connect(
-            database=db,
+        connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+            pool_name="streakbot_pool",
+            pool_size=5,
+            pool_reset_session=True,
             host=host_name,
+            database=db,
             user=user_name,
-            passwd=user_password
-        )
+            password=user_password)
         logging.info('Connection to MySQL DB successful')
+        return connection_pool
     except Error as e:
-        logging.exception(f'The error {e} occurred')
+        logging.exception(f'Error while connecting to MySQL using Connection pool {e}')
         sys.exit(1)
-    return connection
 
 try:
     with open('creds.json', 'r') as file:
@@ -37,7 +43,8 @@ except:
     sys.exit(1)
 
 bot = commands.Bot(command_prefix='$')
-db_conn = create_db_connection('localhost', creds['mysql']['user'], creds['mysql']['pass'], 'streakbot')
+db_pool = create_db_connpool('localhost', creds['mysql']['user'], creds['mysql']['pass'], DB_NAME)
+streak = Streaks(db_pool, CMD_COOLDOWN)
 
 @bot.event
 async def on_ready():
@@ -55,13 +62,12 @@ async def on_message(message):
 
 @bot.command()
 async def ping(ctx):
-    #user = discord.utils.get(ctx.message.server.members, id = '149435714684059648')
-    #await ctx.send(f'*heavy breathing*, you called, {ctx.message.author.name}? Your ID is {ctx.message.author.id}. This is a test ping with the ID <@!149435714684059648>')
     await ctx.send(f'Pong! Latency is {round(bot.latency, 1)}')
 
 
 @bot.event
 async def on_command_error(ctx, error):
+    error = getattr(error, "original", error)
     if isinstance(error, commands.CommandOnCooldown):
         min, sec = divmod(error.retry_after, 60)
         hour, min = divmod(min, 60)
@@ -71,25 +77,40 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-@commands.cooldown(1, 86400, commands.BucketType.user)
+async def hug(ctx, arg: discord.Member = None):
+    if arg is not None:
+        await ctx.send(f'Sending hugs to <@!{arg.id}> <:takenrg:670936332822118420>')
+    else:
+        await ctx.send('Hugs for who?')
+
+
+@hug.error
+async def hug_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send("I don't know who that is.")
+    else:
+        raise error
+
+
+@bot.command()
 # Will throw CommandOnCooldown error if on CD
 async def daily(ctx):
-    if streak.check_user_exists(ctx.message.author.id, ctx.message.author, db_conn):
-        streak_success = streak.set_streak(ctx.message.author.id, db_conn)
+    if streak.check_user_exists(ctx.message.author.id, ctx.message.author):
+        streak_success = streak.set_streak(ctx.message.author.id)
         if streak_success is True:
-            await ctx.send(f'Daily set for {ctx.message.author} - your current streak is {streak.get_streak(ctx.message.author.id, db_conn)}')
+            await ctx.send(f'Daily set for {ctx.message.author} - your current streak is {streak.get_streak(ctx.message.author.id)}')
         elif streak_success is False:
             await ctx.send('More than 48 hours have passed since you last claimed your daily, your streak has been reset to 1')
         else:
             await ctx.send('Something went wrong setting your daily, try again later')
     else:
-        daily.reset_cooldown(ctx)
         await ctx.send(f'Could not update daily for {ctx.message.author}')
+
 
 @bot.command()
 async def leaderboard(ctx):
-    streak.timeout_streaks(db_conn)
-    leaderboard = streak.get_leaderboard(db_conn)
+    streak.timeout_streaks()
+    leaderboard = streak.get_leaderboard()
     counter = 1
     leaderboard_text = ''
     for user, curr_streak in leaderboard:
@@ -98,17 +119,43 @@ async def leaderboard(ctx):
         counter += 1
 
     embed = discord.Embed(color=0x00bfff)
-    #embed.set_author(name='Streak Leaderboard',icon_url=ctx.guild.icon_url)
     embed.set_thumbnail(url=ctx.guild.icon_url)
     embed.add_field(name='Streak Leaderboard', value=leaderboard_text, inline=True)
     embed.set_footer(text=f'Increase your streak by drawing each day and using {bot.command_prefix}daily!')
     await ctx.send(embed=embed)
 
-    #embed.add_field(name="Field1", value="hi", inline=False)
-    #embed.add_field(name="Field2", value="hi2", inline=False)
-    #await message.channel.send(embed=embed)
+
+@bot.command()
+async def pb(ctx, arg: discord.Member = None):
+    if arg is None:
+        personal_best = streak.get_pb_leaderboard()
+        counter = 1
+        pb_leaderboard_text = ''
+        for user, pb in personal_best:
+            username = bot.get_user(int(user))
+            pb_leaderboard_text += f'**{counter}.** {username}  -  {pb}\n'
+            counter += 1
+
+        embed = discord.Embed(color=0x00bfff)
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        embed.add_field(name='Personal Best Leaderboard',
+                        value=pb_leaderboard_text, inline=True)
+        embed.set_footer(text=f'Set new records by drawing each day and using {bot.command_prefix}daily!')
+        await ctx.send(embed=embed)
+    else:
+        personal_best = streak.get_user_pb(arg.id)[0]
+        await ctx.send(f'Current personal best for {arg} is {personal_best}')
+
+
+@pb.error
+async def pb_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send("Unable to look up personal best for that user.")
+    else:
+        raise error
+
 try:
     bot.run(creds['discord']['token'])
-except:
-    logging.exception('Could not connect to Discord')
+except Exception as e:
+    logging.exception(f'Could not connect to Discord {e}')
     sys.exit(1)
--- streak.py
+++ streak.py
@@ -1,118 +1,178 @@
-import discord
 from discord.ext import commands
 from datetime import datetime
-import mysql.connector
 from mysql.connector import Error
 import logging
 import sys
 
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', stream=sys.stderr,level=logging.INFO)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    stream=sys.stderr, level=logging.INFO)
 
-def create_user(user_id, fulluser, db_conn):
-    try:
-        cursor = db_conn.cursor()
-        query = 'INSERT INTO users (user_id, username, discriminator, streak, personal_best) VALUES (%s, %s, %s, %s, %s)'
-        fulluser = str(fulluser)
-        username = fulluser.split('#')[0]
-        discriminator = fulluser.split('#')[1]
-        cursor.execute(query, (user_id, username, discriminator,0,0,))
-        if cursor.rowcount > 0:
-            db_conn.commit()
-            logging.info(f'User {fulluser} created')
-            return True
-        else:
-            raise Exception('No rows to be written')
-    except Error as e:
-        logging.exception(f'Could not create user - {e}')
-        return False
-    finally:
-        cursor.close()
 
-def check_user_exists(user_id, fulluser, db_conn):
-    try:
-        cursor = db_conn.cursor()
-        query = 'SELECT * FROM users WHERE user_id = %s'
-        cursor.execute(query, (user_id,))
-        results = cursor.fetchall()
-        if len(results) == 0:
-            if create_user(user_id, fulluser, db_conn):
+class Streaks:
+    def __init__(self, db_pool, cmd_cd):
+        self.conn_pool = db_pool
+        self.cooldown = cmd_cd
+
+    def create_user(self, user_id, fulluser, db_conn):
+        try:
+            cursor = db_conn.cursor()
+            query = '''INSERT INTO users (user_id, username, discriminator,
+                        streak, personal_best) VALUES (%s, %s, %s, %s, %s)'''
+            fulluser = str(fulluser)
+            username = fulluser.split('#')[0]
+            discriminator = fulluser.split('#')[1]
+            cursor.execute(query, (user_id, username, discriminator, 0, 0,))
+            if cursor.rowcount > 0:
+                db_conn.commit()
+                logging.info(f'User {fulluser} created')
                 return True
             else:
-                return False
-        else:
+                raise Exception('No rows to be written')
+        except Error as e:
+            logging.exception(f'Could not create user - {e}')
+            return False
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+
+    def check_user_exists(self, user_id, fulluser):
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = 'SELECT * FROM users WHERE user_id = %s'
+            cursor.execute(query, (user_id,))
+            results = cursor.fetchall()
+            if len(results) == 0:
+                if self.create_user(user_id, fulluser, db_conn):
+                    return True
+                else:
+                    return False
+            else:
+                return True
+        except Error as e:
+            logging.exception(f'Could not check users table - {e}')
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
+
+    def get_streak(self, user_id):
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = 'SELECT streak FROM users WHERE user_id = %s'
+            cursor.execute(query, (user_id,))
+            streak = cursor.fetchone()
+            return streak[0]
+        except Error as e:
+            logging.exception(f'Unable to get streak - {e}')
+            return False
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
+
+    def get_leaderboard(self):
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = '''SELECT user_id, streak FROM users
+                    ORDER BY streak desc LIMIT 10'''
+            cursor.execute(query)
+            results = cursor.fetchall()
+            return results
+        except Error as e:
+            logging.exception(f'Unable to get streak - {e}')
+            return False
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
+
+    def timeout_streaks(self, timeout_duration=172800):
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = '''UPDATE users SET streak = 0 WHERE
+                    TIME_TO_SEC(TIMEDIFF(NOW(), daily_claimed)) >= %s'''
+            cursor.execute(query, (timeout_duration,))
+            db_conn.commit()
+        except Error as e:
+            logging.exception(f'Could not timeout streaks - {e}')
+            return None
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
+
+    def set_streak(self, user_id, timeout_duration=172800):
+        curr_time = datetime.now()
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = 'SELECT daily_claimed FROM users WHERE user_id = %s'
+            cursor.execute(query, (user_id,))
+            results = cursor.fetchone()
+            last_claimed = results[0]
+            if last_claimed is not None:
+                cd_remaining = curr_time - last_claimed
+                cd_remaining = cd_remaining.total_seconds()
+                if cd_remaining <= self.cooldown:
+                    cd_delta = self.cooldown - cd_remaining
+                    raise commands.CommandOnCooldown(self.cooldown, cd_delta)
+                    return None
+                # 2 days without claiming
+                elif cd_remaining >= timeout_duration:
+                    query = '''UPDATE users SET daily_claimed = %s, streak = 1
+                            WHERE user_id = %s'''
+                    cursor.execute(query, (curr_time, user_id,))
+                    db_conn.commit()
+                    return False
+            query = '''UPDATE users SET daily_claimed = %s,
+                    streak = streak + 1, personal_best = CASE WHEN
+                    streak + 1 > personal_best THEN streak
+                    ELSE personal_best END WHERE user_id = %s'''
+            cursor.execute(query, (curr_time, user_id,))
+            db_conn.commit()
             return True
-    except Error as e:
-        logging.exception(f'Could not check users table - {e}')
-    finally:
-        cursor.close()
+        except Error as e:
+            logging.exception(f'Could not update streak - {e}')
+            return None
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
 
-def get_streak(user_id, db_conn):
-    try:
-        cursor = db_conn.cursor()
-        query = 'SELECT streak FROM users WHERE user_id = %s'
-        cursor.execute(query,(user_id,))
-        streak = cursor.fetchone()
-        return streak[0]
-    except Error as e:
-        logging.exception(f'Unable to get streak - {e}')
-        return False
-    finally:
-        cursor.close()
+    def get_user_pb(self, user_id):
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = 'SELECT personal_best FROM users WHERE user_id = %s'
+            cursor.execute(query, (user_id,))
+            results = cursor.fetchone()
+            print(results)
+            return results
+        except Error as e:
+            logging.exception(f'Unable to get personal best - {e}')
+            return False
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
 
-def get_leaderboard(db_conn):
-    try:
-        cursor = db_conn.cursor()
-        query = 'SELECT user_id, streak FROM users ORDER BY streak desc LIMIT 10'
-        cursor.execute(query)
-        results = cursor.fetchall()
-        return results
-    except Error as e:
-        logging.exception(f'Unable to get streak - {e}')
-        return False
-    finally:
-        cursor.close()
-
-def timeout_streaks(db_conn, timeout_duration=172800):
-    try:
-        cursor = db_conn.cursor()
-        query = 'UPDATE users SET streak = 0 WHERE TIME_TO_SEC(TIMEDIFF(NOW(), daily_claimed)) >= %s'
-        cursor.execute(query,(timeout_duration,))
-        db_conn.commit()
-    except Error as e:
-        logging.exception(f'Could not timeout streaks - {e}')
-        return None
-    finally:
-        cursor.close()
-
-def set_streak(user_id, db_conn):
-    curr_time = datetime.now()
-
-    try:
-        cursor = db_conn.cursor()
-        query = 'SELECT daily_claimed FROM users WHERE user_id = %s'
-        cursor.execute(query,(user_id,))
-        results = cursor.fetchone()
-        last_claimed = results[0]
-        if last_claimed is not None:
-            cooldown = curr_time - last_claimed
-            cooldown = cooldown.total_seconds()
-            if cooldown <= 86400:
-                raise commands.CommandOnCooldown(86400, 86400 - cooldown)
-                return None
-            # 2 days without claiming
-            elif cooldown >= 172800:
-                query = 'UPDATE users SET daily_claimed = %s, streak = 1 WHERE user_id = %s'
-                cursor.execute(query,(curr_time,user_id,))
-                db_conn.commit()
-                return False
-        query = '''UPDATE users SET daily_claimed = %s, streak = streak + 1,
-                personal_best = CASE WHEN streak + 1 > personal_best THEN streak
-                ELSE personal_best END WHERE user_id = %s'''
-        cursor.execute(query,(curr_time,user_id,))
-        db_conn.commit()
-        return True
-    except Error as e:
-        logging.exception(f'Could not update streak - {e}')
-        return None
-    finally:
-        cursor.close()
+    def get_pb_leaderboard(self):
+        try:
+            db_conn = self.conn_pool.get_connection()
+            cursor = db_conn.cursor()
+            query = '''SELECT user_id, personal_best FROM users
+                     ORDER BY personal_best desc LIMIT 10'''
+            cursor.execute(query)
+            results = cursor.fetchall()
+            return results
+        except Error as e:
+            logging.exception(f'Unable to get personal best - {e}')
+            return False
+        finally:
+            if(db_conn.is_connected()):
+                cursor.close()
+                db_conn.close()
